97b87331adde5c4cb92e9eb81b0766d4cbfb5f45
jakubkurdziel <jakub.kurdziel@talkie.ai>
2022-12-07 21:05:18 +0100

    add responsive footer

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,22 +1,38 @@
-import styles from "../../styles/Home.module.css";
+import {
+    Box,
+    Container,
+    Stack,
+    Text,
+    useColorModeValue,
+} from '@chakra-ui/react';
 
 export const Footer = () => {
     return (
-        <footer className={styles.footer}>
-            <span>
-                Copyright © 2022 RE:SHRD
-            </span>
+        <Box
+            bg={useColorModeValue('gray.50', 'gray.900')}
+            color={useColorModeValue('gray.700', 'gray.200')}
+            display={'flex'}
+            flexDirection={'column'}
 
-            <a
-                href="https://github.com/kubo550"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-                Powered by{' '}
-                <span className={styles.logo}>
-                    Jakub Kurdziel
-              </span>
-            </a>
-        </footer>
+        >
+            <Box>
+                <Container
+                    as={Stack}
+                    maxW={'6xl'}
+                    py={4}
+                    direction={{base: 'column', md: 'row'}}
+                    spacing={4}
+                    justify={{base: 'center', md: 'space-between'}}
+                    align={{base: 'center', md: 'center'}}>
+                    <Text>Copyright © 2022 RE:SHRD</Text>
+                    <Text display={{default: 'none', md: 'block'}} >
+                        Powered by{' '} <a href="https://github.com/kubo550"
+                                           target="_blank"
+                                           rel="noopener noreferrer">Jakub Kurdziel</a>
+                    </Text>
+
+                </Container>
+            </Box>
+        </Box>
     );
 }
