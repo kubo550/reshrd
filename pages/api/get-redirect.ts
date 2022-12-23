488e18001f5386fa1e9b8e8666e1bdb1705549ac
jakubkurdziel <jakub.kurdziel@talkie.ai>
2022-12-23 13:32:25 +0100

    enable cors policy

--- pages/api/get-redirect.ts
+++ pages/api/get-redirect.ts
@@ -1,4 +1,4 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type {NextApiRequest, NextApiResponse} from 'next'
 import {getRedirectUrlByCodeId} from "../../infrastructure/firebase";
 import _ from "lodash";
 
@@ -7,27 +7,32 @@ export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse
 ) {
+    console.log('get-redirect');
     if (req.method !== 'GET') {
-        res.status(405).json({message: 'Method not allowed'});
+        console.log('get-redirect: not GET');
+        return res.status(405).json({message: 'Method not allowed'});
     }
 
     const codeId = req.query.codeId as string;
     console.log('get-redirect', {codeId});
 
     if (!codeId) {
-        res.status(400).json({message: 'Missing codeId'});
+        console.log('get-redirect', 'no codeId');
+        return res.status(400).json({message: 'Missing codeId'});
     }
 
     const redirectUrl = await getRedirectUrlByCodeId(codeId);
 
     if (_.isNull(redirectUrl)) {
-       return res.status(404).json({message: 'Not found'});
+        console.log('get-redirect', 'no redirectUrl');
+        return res.status(404).json({message: 'Not found'});
     }
 
     if (redirectUrl === '') {
-         return res.status(200).json({message: 'No redirect url'});
+        console.log('get-redirect', 'empty redirectUrl');
+        return res.status(200).json({message: 'No redirect url'});
     }
 
 
-    res.status(200).json({ url: redirectUrl });
+    res.status(200).json({url: redirectUrl});
 }
