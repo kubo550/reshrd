b12f02b20ce9141ff8efcb4a174208eac5c31aa0
jakubkurdziel <jakub.kurdziel@talkie.ai>
2024-04-21 12:48:23 +0200

    set cors to registration email

--- pages/api/registration-mail.ts
+++ pages/api/registration-mail.ts
@@ -1,15 +1,26 @@
 import {NextApiRequest, NextApiResponse} from "next";
 import {sendRegistrationEmail} from "../../infrastructure/email-utils";
 
+export const corsHeaders = {
+    "Access-Control-Allow-Origin": "https://my.reshrd.com",
+    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+    "Access-Control-Allow-Headers": "Content-Type, Authorization",
+};
 export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse
 ) {
     try {
+        console.log('registration mail', req.headers.origin)
+
+        if (req.headers.origin !== 'https://my.reshrd.com') {
+            return res.status(403).json({message: 'Forbidden'});
+        }
+
         console.log('registration mail to', req.body.email);
         const email = req.body.email;
-        // await sendRegistrationEmail(email);
-        return res.status(429).json({message: 'disabled for now'});
+        await sendRegistrationEmail(email);
+        return res.status(200).json({message: 'Email sent successfully'});
     } catch (e) {
         console.error(e)
         return res.status(500).json({})
