452a1e385549c9669eb88972b2bcdcea63e7b676
jakubkurdziel <qwercy142@gmail.com>
2025-04-28 19:59:38 +0200

    add google analytics scripts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -47,6 +47,15 @@ export default function App({Component, pageProps}: AppProps) {
     }, [router.events])
 
     return (<>
+            <script
+                dangerouslySetInnerHTML={{
+                    __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                       new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                       j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                       'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                       })(window,document,'script','dataLayer','GTM-FVTD5G8D');`,
+                }}
+            />
             <Head>
                 <title>Updateable QR Clothing Control Panel | RESHRD</title>
                 <meta name="description" content="Changeable QR code clothing management platform | Update your clothes any time you want | Explore limitless possibilities of the RESHRD streetwear and casualwear collections."/>
@@ -61,6 +70,14 @@ export default function App({Component, pageProps}: AppProps) {
                     __html: `window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'G-C96BPLFQXN');`,
                 }}
             />
+            <noscript>
+                <iframe
+                    src="https://www.googletagmanager.com/ns.html?id=GTM-FVTD5G8D"
+                    height="0"
+                    width="0"
+                    style={{ display: 'none', visibility: 'hidden' }}
+                />
+            </noscript>
             <QueryClientProvider client={queryClient}>
                 <AuthContextProvider>
                     <ChakraProvider theme={theme} cssVarsRoot="body">
