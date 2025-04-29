de7c0b580d4feb1291123465de4426ce0bb58d00
jakubkurdziel <qwercy142@gmail.com>
2025-04-29 18:25:54 +0200

    allow to display images from raw.githubusercontent.com

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['cdn.shopify.com'],
+    domains: ['cdn.shopify.com', 'raw.githubusercontent.com'],
   },
   async headers() {
     return [
