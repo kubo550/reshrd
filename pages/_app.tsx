983b877846d6dc3c47c7c52e07b9349c475cb3cb
jakubkurdziel <qwercy142@gmail.com>
2025-04-29 18:25:41 +0200

    move google analytics to _document

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -33,29 +33,12 @@ export default function App({Component, pageProps}: AppProps) {
 
 
     return (<>
-            <script
-                dangerouslySetInnerHTML={{
-                    __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                       new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                       j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                       'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                       })(window,document,'script','dataLayer','GTM-FVTD5G8D');`,
-                }}
-            />
             <Head>
                 <title>Updateable QR Clothing Control Panel | RESHRD</title>
                 <meta name="description" content="Changeable QR code clothing management platform | Update your clothes any time you want | Explore limitless possibilities of the RESHRD streetwear and casualwear collections."/>
                 <link rel="icon" href="/favicon.ico"/>
                 <meta name={'robots'} content={'noindex, nofollow'}/>
             </Head>
-            <noscript>
-                <iframe
-                    src="https://www.googletagmanager.com/ns.html?id=GTM-FVTD5G8D"
-                    height="0"
-                    width="0"
-                    style={{ display: 'none', visibility: 'hidden' }}
-                />
-            </noscript>
             <QueryClientProvider client={queryClient}>
                 <AuthContextProvider>
                     <ChakraProvider theme={theme} cssVarsRoot="body">
